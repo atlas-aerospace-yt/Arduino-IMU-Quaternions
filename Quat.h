6c3cdb2a8e55c1171efea00ef9307e2a69c17d19
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-02-13 23:35:27 +0000

    Update Quat.h

--- Quat.h
+++ Quat.h
@@ -43,14 +43,35 @@
 struct Vect {
     float x, y, z;
 
+    Vect operator/ (float f) {
+      Vect v;
+
+      v.x = x / f;
+      v.y = y / f;
+      v.z = z / f;
+
+      return v;
+    }
+
+    Vect operator* (float f){
+      Vect v;
+
+      v.x = x * f;
+      v.y = y * f;
+      v.z = z * f;
+
+      return v;
+    }
+      
     Vect To_Radians();
     Vect To_Degrees();
 };
 
 struct Quat {
+  
     float w, i, j, k;
     
-    Quat operator/= (float f) {
+    Quat operator/ (float f) {
         Quat q;
 
         q.w = w / f;
@@ -59,6 +80,72 @@ struct Quat {
         q.k = k / f;
 
         return q;
+    }
+    
+    Quat operator* (Quat q){
+      Quat r;
+
+      r.w = w * q.w - i * q.i - j * q.j - k * q.k;
+      r.i = w * q.i + i * q.w + j * q.k - k * q.j;
+      r.j = w * q.j - i * q.k + j * q.w + k * q.i;
+      r.k = w * q.k + i * q.j - j * q.i + k * q.w;
+      
+      return r;
+    }
+
+    Quat operator* (float f){
+      Quat q;
+
+      q.w = w * f;
+      q.i = i * f;
+      q.j = j * f;
+      q.k = k * f;
+
+      return q;
+    }
+    
+    Quat operator+ (Quat q){
+      Quat r;
+
+      r.w = w + q.w;
+      r.i = i + q.i;
+      r.j = j + q.j;
+      r.k = k + q.k;
+
+      return r;
+    }
+
+    Quat operator- (Quat q){
+      Quat r;
+
+      r.w = w - q.w;
+      r.i = i - q.i;
+      r.j = j - q.j;
+      r.k = k - q.k;
+
+      return r;
+    }
+
+    Quat operator+ (float f){
+      Quat r;
+
+      r.w = w + f;
+      r.i = i + f;
+      r.j = j + f;
+      r.k = k + f;
+
+      return r;
+    }
+    
+    Quat operator- (float f){
+      Quat r;
+
+      r.w = w - f;
+      r.i = i - f;
+      r.j = j - f;
+      r.k = k - f;
+
+      return r;
     }
 
     Quat Update(Vect v, float dt);
