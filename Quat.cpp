63d339451176adb2136e5ccf1e3eee477b969f4c
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-04-22 23:24:29 +0100

    Update Quat.cpp

--- Quat.cpp
+++ Quat.cpp
@@ -36,9 +36,12 @@
 * https://www.youtube.com/channel/UCWd6oqc8nbL-EX3Cxxk8wFA
 */
 
-#include "Quat.h"
+// WARNING: USE AT OWN RISK, THIS CODE HAS NOT BEEN FULLY TESTED
 
-Vect Vect::To_Radians(){
+#include "Quat.h"
+#include "Arduino.h"
+
+Vect Vect::To_Radians() {
   Vect ypr;
 
   ypr.x = x * (PI / 180.0f);
@@ -48,7 +51,7 @@ Vect Vect::To_Radians(){
   return ypr;
 }
 
-Vect Vect::To_Degrees(){
+Vect Vect::To_Degrees() {
   Vect ypr;
 
   ypr.x = x * (180.0f / PI);
@@ -58,23 +61,46 @@ Vect Vect::To_Degrees(){
   return ypr;
 }
 
+
+// Quaternions from angular rate using madgwick paper
 Quat Quat::Update(Vect v, float dt) {
 
-    Quat b = {0, v.x, v.y, v.z};
-    Quat r = {w, i, j, k};
+  Quat b = {0, v.x, v.y, v.z};
+  Quat r = {w, i, j, k};
 
-    Quat q = r * 0.5 * b; 
-    Quat n = r + q * dt;
-    
-    return n;
+  Quat q = r * 0.5 * b;
+  Quat n = r + (q * dt);
+
+  n.norm = sqrt(n.w * n.w + n.i * n.i + n.j * n.j + n.k * n.k);
+
+  n = n / n.norm;
+
+  return n;
 }
 
-Vect Quat::To_Euler() {
-    Vect ypr;
+// Euler angles from quaternions
+Vect Quat::To_Euler()
+{
+  Vect ypr;
 
-    ypr.x = atan2(2 * i * j - 2 * w * k, 2 * w * w + 2 * i * i - 1);
-    ypr.y = -asin(2 * i * k + 2 * w * j);
-    ypr.z = atan2(2 * j * k - 2 * w * i, 2 * w * w + 2 * k * k - 1);
-    
-    return ypr; 
+  // X-Axis calculations
+  float x_one  = 2 * i * j - 2 * w * k;
+  float x_two = 2 * w * w + 2 * i * i - 1;
+  ypr.x = atan2(x_one, x_two);
+
+  // Y_Axis calculations
+  float y = 2 * i * k + 2 * w * j;
+  if (y > 1.0f){
+    ypr.y = -asin(PI * 2 / y); // uses 90 as a failsafe
+  }
+  else{
+    ypr.y = -asin(y);
+  }
+
+  // Z-Axis calculations
+  float z_one = 2 * j * k - 2 * w * i;
+  float z_two = 2 * w * w + 2 * k * k - 1;
+  ypr.z = atan2(z_one, z_two);
+
+  return ypr;
 }
